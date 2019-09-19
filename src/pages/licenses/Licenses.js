85128059edd68286394073f7e708a7c2978b900b
Gerolf Seitz <gerolf.seitz@digitalasset.com>
2019-09-19 20:44:26 +0200

    Show reseller/reader for the respective opposite party

--- src/pages/licenses/Licenses.js
+++ src/pages/licenses/Licenses.js
@@ -1,20 +1,25 @@
 import React from "react";
 import PageTitle from "../../components/PageTitle/PageTitle";
-import { useLedgerState, getContracts } from "../../context/LedgerContext";
+import { useLedgerState, getContracts, getContract } from "../../context/LedgerContext";
 import Contracts from "../../components/Contracts/Contracts";
 
 function Licenses({ history }) {
 
   const ledger = useLedgerState();
-  const licenses = getContracts(ledger, "Main", "BookLicense");
-  
+  const licenses = getContracts(ledger, "Book", "BookLicense");
+  const isReseller = !!getContract(ledger, "Book", "Reseller");
+  function resellerOrReaderColumn() {
+    if (isReseller) return [ "Reader", "argument.reader" ]
+     return ["Reseller", "argument.reseller"]
+  }
+
   return (
     <>
       <PageTitle title="Licenses"/>
       <Contracts
         contracts={licenses}
         columns={[
-          [ "Reader", "argument.reader" ],
+          resellerOrReaderColumn(),
           [ "ISBN", "argument.book.isbn"],
           [ "Title", "argument.book.title" ],
           [ "Author", "argument.book.author" ]
