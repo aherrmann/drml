13b357d9ecef5e8711e959e33a3401922c0a3123
Matthias Schmalz <matthias.schmalz@digitalasset.com>
2019-09-20 11:11:09 +0200

    Unnecessary statement removed

--- 1_sandbox_start.sh
+++ 1_sandbox_start.sh
@@ -2,9 +2,6 @@
 
 set -eo pipefail
 
-# Change to the project base directory
-cd $(dirname "${BASH_SOURCE[0]}")
-
 # This will install both run-time project dependencies and developer tools listed in package.json.
 ./install.sh
 
